8e46f02d9db6e639303433988e342ae89c910e30
Dean Harris <dean@harris.tc>
2021-09-05 12:46:27 +1000

    test(commitlint): move tests from js to ts

--- packages/commitlint/src/test/header.spec.ts
+++ packages/commitlint/src/test/header.spec.ts
@@ -1,10 +1,13 @@
 import lint from '@commitlint/lint';
+import type { RuleConfigQuality, RulesConfig } from '@commitlint/types';
 import config from '..';
+
+const rules = config.rules as Partial<RulesConfig<RuleConfigQuality.Qualified>>;
 
 describe('Commit Header', () => {
   it('should fail due to being too short', async () => {
     const commit = 'fix: short msg';
-    const result = await lint(commit, config.rules);
+    const result = await lint(commit, rules);
     expect(result.valid).toBe(false);
     expect(result.errors[1].message).toBe(
       `header must not be shorter than 15 characters, current length is ${commit.length}`,
@@ -13,7 +16,7 @@ describe('Commit Header', () => {
 
   it('should fail due to being too long', async () => {
     const commit = 'delete(scope): some super duper long commit message';
-    const result = await lint(commit, config.rules);
+    const result = await lint(commit, rules);
     expect(result.valid).toBe(false);
     expect(result.errors[0].message).toBe(
       `header must not be longer than 50 characters, current length is ${commit.length}`,

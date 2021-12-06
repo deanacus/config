264a705efe0e703039b9769ddd8a7d72bce90174
Dean Harris <dean@harris.tc>
2021-12-06 14:30:16 +1000

    feat(eslint): update version of eslint

--- packages/eslint/src/utils/getSettings.ts
+++ packages/eslint/src/utils/getSettings.ts
@@ -1,5 +1,5 @@
 import { Linter } from 'eslint';
-import { hasTypescript } from '.';
+import { hasReact, hasTypescript } from '.';
 
 export const getSettings = (): Linter.Config['settings'] => ({
   'import/resolver': {
@@ -7,5 +7,12 @@ export const getSettings = (): Linter.Config['settings'] => ({
       extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     ...(hasTypescript ? { typescript: {} } : null),
+    ...(hasReact
+      ? {
+          react: {
+            version: 'detect',
+          },
+        }
+      : null),
   },
 });
